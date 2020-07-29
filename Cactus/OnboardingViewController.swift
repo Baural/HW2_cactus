3d6d63ea0cc7d8e90a930c58045bfe3a508f4a36
baur <alzhanovbb@gmail.com>
2020-07-29 22:24:42 +0600

    Onboarding working

--- Cactus/OnboardingViewController.swift
+++ Cactus/OnboardingViewController.swift
@@ -10,17 +10,47 @@ import UIKit
 
 class OnboardingViewController: UIViewController {
     @IBOutlet var textLabel: UILabel!
-    
-    let titles = ["Welcome\nto the\nCactus app!", "Choose\ncactus\nto plant.", "Stay focused\n& productive!"]
-    
+    @IBOutlet var nextButton: UIButton!
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var pageControl: UIPageControl!
+   
     var currentIndex = 0
+    
+    let titles = ["Welcome\nto the\nCactus app!",
+    "Choose\ncactus\nto plant.",
+    "Stay focused\n& productive!"]
+    
+    let imageNames = ["onboarding_welcome",
+                      "onboarding_choose",
+                      "onboarding_stay"]
+    
+  override func viewDidLoad() {
+           super.viewDidLoad()
+           
+           nextButton.layer.cornerRadius = 14
+           nextButton.layer.masksToBounds = true
+           
+           updateInterface()
+       }
+    
+    @IBAction func didTapNextButton() {
+            if currentIndex == 2 {
+            performSegue(withIdentifier: "showSession", sender: nil)
+        } else {
+            currentIndex += 1
+            pageControl.currentPage = currentIndex
+            updateInterface()
+        }
+    
+    
+    }
     
     func updateInterface() {
         textLabel.text = titles[currentIndex]
-//        imageView.image = UIImage(named: imageNames[currentIndex])
-//
-//        if currentIndex == 2 {
-//            nextButton.setTitle("Start", for: .normal)
-//        }
+        imageView.image = UIImage(named: imageNames[currentIndex])
+
+        if currentIndex == 2 {
+            nextButton.setTitle("Start", for: .normal)
+        }
     }
 }
